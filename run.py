bef4f51dbb36ab018d81ea574edff8f512ace0e4
Linxzh <linxzh1989@gmail.com>
2022-04-16 22:17:13 +0800

    Fix error

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ import requests
 token = os.getenv("TOKEN")
 headers = {
     "authorization": "Bearer %s" % TOKEN,
-    'content-type": "application/json"
+    "content-type": "application/json"
 }
+
 resp = requests.get("https://api.github.com/repos/l0o0/jasminum", headers=headers)
 print(resp.json)
